2b8900b76959124801922393739b265d96634d4e
mkeblx <mkeblx@gmail.com>
2021-01-16 07:30:03 +0000

    Add url param handling

--- index.js
+++ index.js
@@ -10,19 +10,36 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api', async (req, res) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
-  let title = await page.title();
-  //await page.screenshot({path: 'example.png'});
+  let url = req.query.url;
 
-  await browser.close();
+  let resp = {};
 
-  let resp = {
-    msg: 'model found',
-    url: 'http://site.com/model.glb',
-    title: title
-  };
+  if (url) {
+    if (!url.startsWith('http')) {
+      url = 'http://' + url;
+    }
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    let title = await page.title();
+    //await page.screenshot({path: 'example.png'});
+
+
+    await browser.close();
+
+    resp = {
+      msg: 'model found',
+      url: 'http://site.com/model.glb',
+      title: title
+    };
+  } else {
+    resp = {
+      msg: "'url' parameter not provided"
+    };
+  }
   res.json(resp);
 });
 
